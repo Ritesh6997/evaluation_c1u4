55588364eb2b9fd6c7c146ac9d508fa8248e5d5e
Ritesh6997 <96717182+Ritesh6997@users.noreply.github.com>
2022-03-07 12:45:01 +0530

    submitting

---
+++ index.js
@@ -0,0 +1,35 @@
+const express=require("express");
+const app=express();
+
+app.get("/books",(req,res)=>{
+    return res.send ("printing all the books");
+})
+
+app.get("/libraries",checkPermissions("librarian"),(req,res)=>{
+    return res.send(req.permission);
+});
+app.get("/authors",checkPermissions("author"),(req,res)=>{
+    return res.send(req.permission);
+})
+
+function checkPermissions(x){
+    return function logger(req,res,next){
+        if (x=="librarian")
+        {
+            req.permission=true;
+        }
+        else if (x=="author"){
+            req.permission=true;
+        }
+        next()
+    }
+}
+
+
+function  logger1(req,res,next){
+    next();
+}
+
+app.listen(5000,()=>{
+    console.log("listing to port 5000");
+})
